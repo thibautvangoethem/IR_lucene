d8cff9bc5f9d93dfb9980448cf20278ea0ece863
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2020-10-20 11:20:04 +0200

    first attempt at jaxb xml

---
+++ src/main/java/xml/Row.java
@@ -0,0 +1,22 @@
+package xml;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType (XmlAccessType.FIELD)
+public class Row {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
---
+++ src/main/java/xml_parser/XmlToPostgres.java
@@ -0,0 +1,34 @@
+package xml_parser;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import xml.Posts;
+import xml.Row;
+
+public class XmlToPostgres {
+	public static void main(String[] args) {
+		try {    
+			URL resource = XmlToPostgres.class.getClassLoader().getResource("small.xml");
+            File file = new File(resource.toURI());    
+            JAXBContext jaxbContext = JAXBContext.newInstance(Posts.class);    
+         
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();    
+            Posts e=(Posts) jaxbUnmarshaller.unmarshal(file);    
+            for(Row row:e.getRows()) {
+            	System.out.print(row.getId());
+            }
+            
+          } catch (JAXBException e) {e.printStackTrace(); } catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}    
+         
+	}
+
+}
